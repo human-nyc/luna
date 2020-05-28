5e48c956d7a7411a5d8ad361be2b009ab12e638b
Matthew Kandler <matthew.kandler@gmail.com>
2020-05-28 13:36:16 -0400

    Press slider works on mobile too

--- scripts/sections/press.js
+++ scripts/sections/press.js
@@ -1,4 +1,7 @@
+import Flickity from 'flickity';
 import Vue from 'vue';
+
+let flkty;
 
 const press = new Vue({
   delimiters: ['${', '}'],
@@ -19,18 +22,54 @@ const press = new Vue({
   },
   methods: {
     selectItem: function(index) {
+      // User input, clear interval
+      if (index === this.activeItem) {
+        return;
+      }
+      clearInterval(this.interval);
+      this.preventReset = false;
+      this.changeItem(index);
+    },
+    changeItem: function(index) {
       this.inactiveItem = this.activeItem;
       setTimeout(() => {
         this.inactiveItem = null;
       }, 750);
       this.activeItem = index;
+
+      if (flkty && index !== flkty.selectedIndex) {
+        flkty && flkty.select(index);
+      }
     },
     nextItem: function() {
       let activeItem = this.activeItem + 1;
       if (activeItem >= this.count) {
         activeItem = 0;
       }
-      this.selectItem(activeItem);
+      this.changeItem(activeItem);
     },
   },
+});
+
+flkty = new Flickity('#pressSlider', {
+  pageDots: false,
+  prevNextButtons: false,
+  fade: true,
+  autoPlay: 0,
+  initialIndex: 0,
+  pauseAutoPlayOnHover: true,
+  arrowShape: {
+    x0: 10,
+    x1: 60, y1: 50,
+    x2: 70, y2: 40,
+    x3: 30
+  },
+  on: {
+    ready: () => {
+
+    },
+    change: (index) => {
+      press.selectItem(index);
+    },
+  }
 });
--- scripts/snippets/product-slider.js
+++ scripts/snippets/product-slider.js
@@ -1,10 +1,34 @@
 import Vue from 'vue';
+import Flickity from 'flickity';
+
+var flkty = new Flickity('#productSlider', {
+  pageDots: false,
+  prevNextButtons: true,
+  fade: true,
+  autoPlay: 5000,
+  initialIndex: 1,
+  pauseAutoPlayOnHover: true,
+  arrowShape: {
+    x0: 10,
+    x1: 60, y1: 50,
+    x2: 70, y2: 40,
+    x3: 30
+  },
+  on: {
+    ready: () => {
+
+    },
+    change: () => {
+    },
+  }
+});
 
 const ProductOptions = Vue.component('product-options', {
-  // props: ['product'], // declare the props
+  props: ['product'], // declare the props
   template: `<p>I\'m the options.</p>`,
   mounted: function () {
     // Do Something?
+    console.log('Options for product: ', this.product);
   }
 });
 
@@ -13,6 +37,11 @@ const product = new Vue({
   el: '.product-item',
   data: {
     message: 'Hi',
+  },
+  methods: {
+    addToCart: function() {
+
+    }
   },
   components: {
     ProductOptions,
