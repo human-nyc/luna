6c9b1a92b36a23790d07663681f9188934c09b17
Matthew Kandler <matthew.kandler@gmail.com>
2020-05-28 11:48:48 -0400

    Cleaning up testimonials

--- scripts/sections/testimonials.js
+++ scripts/sections/testimonials.js
@@ -7,6 +7,12 @@ var flkty = new Flickity('#testimonials', {
   autoPlay: 5000,
   initialIndex: 1,
   pauseAutoPlayOnHover: true,
+  arrowShape: {
+    x0: 10,
+    x1: 60, y1: 50,
+    x2: 70, y2: 40,
+    x3: 30
+  },
   on: {
     ready: () => {
 
