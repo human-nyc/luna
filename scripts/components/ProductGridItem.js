4e12e486cfa6ba2c2419038200bec72125f49fc5
michaelburtonray <michael@human-nyc.com>
2020-07-24 17:11:01 -0400

    wip

--- scripts/components/ProductGridItem.js
+++ scripts/components/ProductGridItem.js
@@ -4,26 +4,31 @@ const ProductGridItem = Vue.component('product-grid-item', {
   props: ['product-json','options-with-values-json', 'first-available-variant-id'],
   methods: {
     respondToChange() {
-    }
+    },
   },
   data() {
     return {
-      option: {},
-      selectedVariant: {}
+      product: {},
+      selectedVariant: {
+        1: null,
+        2: null,
+        3: null,
+      },
     }
   },
   mounted() {
-
+    this.product =  JSON.parse(this.productJson);
+    // this.selectedVariant = this.product.variants.find(variant => variant.available) ? this.product.variants.find(variant => variant.available) : this.product.variants[0];
+    // this.options.option1 = this.selectedVariant.option1;
+    // this.options.option2 = this.selectedVariant.option2;
+    // this.options.option3 = this.selectedVariant.option3;
   },
   computed: {
-    options() {
-      return 'hi';
-    },
     inputOptionAttributes() {
       return (product, option, value) => ({
-        name: `option[${option.name}]`,
-        value: value,
-        id: `"product${product.id}_option${option.name}_value${value}`,
+        name: `option${option.position}`,
+        id: `"product${product.id}_option${option.name}_value${value.replace(' ', '-')}`,
+        key: `"product${product.id}_option${option.name}_value${value.replace(' ', '-')}`
       })
     },
     labelOptionAttributes() {
@@ -32,43 +37,30 @@ const ProductGridItem = Vue.component('product-grid-item', {
         class: `option option--${option.name}`
       })
     },
+    options: {
+      get() {
+        return {
+          1: this.selectedVariant.option1,
+          2: this.selectedVariant.option2,
+          3: this.selectedVariant.option3,
+        }
+      },
+      set(value, a, b, c) {
+        alert(value);
+        console.log(value, a, b, c);
+        return
+      },
+    },
     optionsWithValues() {
       return JSON.parse(this.optionsWithValuesJson)
     },
-    product() {
-      return JSON.parse(this.productJson)
-    },
     selectedOrFirstAvailableVariantId: {
       get() {
-        return this.product.variants[1].id
+        return
       },
       set() {
         return
       },
-    },
-    variantId: {
-      get() {
-        const form = this.$el.querySelector('form');
-        var options = [];
-
-        if(form.Option1) {
-          options.push(form.Option1.value);
-        }
-
-        if(form.Option2) {
-          options.push(form.Option2.value)
-        }
-
-        if(form.Option3) {
-          options.push(form.Option3.value)
-        }
-
-        const variantId = this.product.variants.find(variant => {
-          return variant.public_title = options.join(" / ")
-        }).id;
-
-        return variantId
-      }
     },
   },
   filters: {
@@ -76,8 +68,8 @@ const ProductGridItem = Vue.component('product-grid-item', {
       if (!value) return '';
       value = value.toString();
       return value.replace(' ', '-').toLowercase();
-    }
-  }
+    },
+  },
 })
 
 export default ProductGridItem;
