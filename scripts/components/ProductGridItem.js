62445e8da99c14ab7a26c93dadbb30d275833595
michaelburtonray <michael@human-nyc.com>
2020-07-29 17:14:03 -0400

    quickbuy work in progress

--- scripts/components/ProductGridItem.js
+++ scripts/components/ProductGridItem.js
@@ -1,19 +1,32 @@
 import Vue from 'vue';
+import { getSizedImageUrl } from "@shopify/theme-images";
+
 
 const ProductGridItem = Vue.component('product-grid-item', {
   props: ['product-json','options-with-values-json', 'first-available-variant-id'],
   methods: {
+    activateQuickAdd() {
+      this.quickAddIsActive = true;
+    },
     respondToChange() {
     },
+    handleOptionChange({optionIdx}) {
+      console.log({optionIdx});
+
+      // this.options.forEach((option, idx) => {
+      //   if(idx > optionIdx) {
+      //     this.options[idx] = undefined;
+      //   }
+      // });
+
+      this.options = this.options.filter((option, idx) => idx <= optionIdx);
+    }
   },
   data() {
     return {
       product: {},
-      selectedVariant: {
-        1: null,
-        2: null,
-        3: null,
-      },
+      options: [],
+      quickAddIsActive: false
     }
   },
   mounted() {
@@ -34,41 +47,41 @@ const ProductGridItem = Vue.component('product-grid-item', {
     labelOptionAttributes() {
       return (product, option, value) => ({
         for: `"product${product.id}_option${option.name}_value${value.replace(' ', '-')}`,
-        class: `option option--${option.name}`
+        class: `option option--${option.name.toLowerCase()} option--${value.replace(' ', '-').toLowerCase()}`
       })
-    },
-    options: {
-      get() {
-        return {
-          1: this.selectedVariant.option1,
-          2: this.selectedVariant.option2,
-          3: this.selectedVariant.option3,
-        }
-      },
-      set(value, a, b, c) {
-        alert(value);
-        console.log(value, a, b, c);
-        return
-      },
     },
     optionsWithValues() {
       return JSON.parse(this.optionsWithValuesJson)
     },
-    selectedOrFirstAvailableVariantId: {
+    selectedVariant: {
       get() {
-        return
+        if(this.product.variants) {
+          var result = this.product.variants.filter(variant => {
+            return this.options.every((option, index) => {
+              return variant.options[index] === option;
+            });
+          });
+
+          return result[0] || null;
+        } else {
+          return { id: 'stevie' };
+        }
       },
-      set() {
-        return
-      },
+      set(value) {
+        return this.product.variants.find(variant => variant.id == value);
+      }
     },
   },
   filters: {
     handleize(value) {
       if (!value) return '';
       value = value.toString();
-      return value.replace(' ', '-').toLowercase();
+      return value.replace(' ', '-').toLowerCase();
     },
+    getSizedImageUrl(url, size) {
+      console.log({url, size});
+      return getSizedImageUrl(url, size);
+    }
   },
 })
 
