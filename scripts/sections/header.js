8257398b56e7b451e99af5ef724ee51a1fe2616d
michaelburtonray <michael@human-nyc.com>
2020-07-30 16:20:10 -0400

    more vue like. mobile menu

--- scripts/sections/header.js
+++ scripts/sections/header.js
@@ -1,10 +1,12 @@
 import Vue from 'vue';
 
 const header = new Vue({
+  name: "Header",
   delimiters: ['${', '}'],
   el: '#headerComponent',
   data: {
     message: 'Hello Vue!',
+    mobileMenuIsOpen: false,
     isScrolled: false,
     cartCount: 0,
     activeChildLinksIndex: -1
@@ -18,29 +20,17 @@ const header = new Vue({
   methods: {
     handleScroll: function() {
       this.isScrolled = window.scrollY > 50;
-
-      this.$el.querySelectorAll('.header__dropdown').forEach((el, idx) => {
-        el.classList.toggle('active', false);
-      });
+      this.activeChildLinksIndex = -1;
     },
     toggleActiveChildLinks({ activeChildLinksIndex }) {
       if(this.activeChildLinksIndex == activeChildLinksIndex) {
         this.activeChildLinksIndex = -1
       } else {
-        this.activeChildLinksIndex = 0;
+        this.activeChildLinksIndex = activeChildLinksIndex
       }
-      // if(activeChildLinksIndex === this.activeChildLinksIndex) {
-      //   this.activeChildLinksIndex = -1;
-      //   this.$el.querySelector('.header__dropdown.active').classList.remove('active');
-      // } else {
-      //   this.$el.querySelectorAll('.header__dropdown').forEach((el, idx) => {
-      //     el.classList.toggle('active', idx == activeChildLinksIndex);
-      //   });
-      //   this.activeChildLinksIndex = activeChildLinksIndex;
-      // }
     },
-    toggleOpen: function() {
-      this.isOpen = !this.isOpen;
+    toggleMobileMenu: function() {
+      this.mobileMenuIsOpen = !this.mobileMenuIsOpen;
     },
   },
 });
