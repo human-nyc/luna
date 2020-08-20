635488ce935f7dcef1f3a5fd623cec6eb969294f
Matthew Kandler <matthew.kandler@gmail.com>
2020-08-20 15:55:36 -0400

    Adds optional extra images for the product steps section

--- scripts/snippets/product-steps.js
+++ scripts/snippets/product-steps.js
@@ -5,10 +5,16 @@ const productSteps = new Vue({
   el: '#ProductSteps',
   data: {
     step: 1,
+    imageCount: 0,
+    activeImage: 1,
+  },
+  created() {
+    this.imageCount = document.querySelectorAll('.product-steps__image img').length;
   },
   methods: {
     selectStep: function(step) {
       this.step = step;
+      this.activeImage = step <= this.imageCount ? step : this.imageCount;
     },
   },
 });
