1696cf13a2f0df218bb62dde2b73e373d8f69591
Rinaldy Laguerre <26579000+rinaldylaguerre@users.noreply.github.com>
2020-08-25 12:48:23 -0400

    set up mixins to reuse functions

--- scripts/apps/Product.js
+++ scripts/apps/Product.js
@@ -5,6 +5,7 @@ import querystring from 'querystring';
 import { mapActions } from 'vuex';
 
 import store from '../store';
+import productOptions from '../mixins/productOptions';
 
 document.addEventListener('DOMContentLoaded', () => {
   if (document.querySelector('#ProductHero')) {
@@ -37,6 +38,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         this.optionsWithValues = JSON.parse(this.$el.dataset.optionsWithValues);
       },
+      mixins: [productOptions],
       methods: {
         ...mapActions('cart', ['addToCart', 'hydrateCartItems', 'toggleMiniCart']),
 
@@ -50,12 +52,6 @@ document.addEventListener('DOMContentLoaded', () => {
           await this.addToCart(cartData);
           await this.hydrateCartItems();
           this.toggleMiniCart();
-        },
-
-        handleOptionChange({ optionIdx }) {
-          this.options = this.options.filter((option, idx) => {
-            return idx <= optionIdx
-          });
         },
 
         activateOption(optionIdx) {
@@ -112,109 +108,7 @@ document.addEventListener('DOMContentLoaded', () => {
         },
       },
       computed: {
-        selectedVariant: {
-          get() {
-            if (this.product.variants) {
-              let result = this.product.variants.filter(variant => {
-                return this.options.every((option, index) => {
-                  return variant.options[index] === option;
-                });
-              });
 
-              if (result.length > 0) {
-                let getVars = querystring.parse(location.search.substr(1));
-                getVars['variant'] = result[0].id;
-
-                const url = `${window.location.pathname}?${querystring.stringify(getVars)}`;
-                history.replaceState({}, '', url);
-              }
-
-              return result[0] || null;
-            } else {
-              return { id: null, price: 0, compare_at_price: 0, featured_image: { src: '' } };
-            }
-          },
-          set(value) {
-            return this.product.variants.find(variant => variant.id == value);
-          }
-        },
-
-        optionAttributes() {
-          return (option, optionIdx) => ({
-            class: [
-              'product_option',
-              `option--${option.name.toLowerCase()}`,
-              { active: optionIdx == this.activeOptionIdx }
-            ]
-          })
-        },
-
-        inputOptionAttributes() {
-          return (product, option, value) => ({
-            name: `option${option.position}`,
-            id: `product${product.id}_option${option.name}_value${value.replace(' ', '-')}`,
-            key: `"product${product.id}_option${option.name}_value${value.replace(' ', '-')}`
-          })
-        },
-
-        labelOptionAttributes() {
-          return (product, option, value) => ({
-            for: `product${product.id}_option${option.name}_value${value.replace(' ', '-')}`,
-            class: `option-value  option--${value.replace(' ', '-').toLowerCase()}`
-          })
-        },
-
-        potentialVariants() {
-          if (this.product.variants) {
-            let result = this.product.variants.filter(variant => {
-              return this.opttions.every((option, index) => {
-                return option === variant.options[index];
-              });
-            });
-
-            return result;
-          } else {
-            return null;
-          }
-        },
-
-        potentialOptions1() {
-          if (this.options[0] && this.product.variants) {
-            return new Set(this.product.variants.reduce((acc, curr) => {
-              if (curr.options[0] === this.options[0]) acc.push(curr.options[1]);
-              return acc
-            }, []));
-          }
-
-          return new Set(this.potentialVariants.map(variant => variant.options[1]));
-        },
-
-        potentialOptions2() {
-          if (this.options[1] && this.product.variants) {
-            return new Set(this.product.variants.reduce((acc, curr) => {
-              if (
-                curr.options[0] === this.options[0]
-                && curr.options[1] === this.options[1]
-              ) acc.push(curr.options[2]);
-              return acc
-            }, []));
-          }
-
-          return new Set(this.potentialVariants.map(variant => variant.options[2]));
-        },
-
-        potentialOptions() {
-          if (this.product.variants) {
-            let potentialOptions = [this.optionsWithValues[0].values];
-
-            if (this.options[0]) potentialOptions.push([...this.potentialOptions1])
-            if (this.options[1]) potentialOptions.push([...this.potentialOptions2].sort());
-
-            return potentialOptions;
-          } else {
-            return null;
-          }
-        }
       },
 
       filters: {
