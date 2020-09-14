bed10e519ff7a46bcf453f2594bb14e1949127de
michaelburtonray <michael@human-nyc.com>
2020-09-14 17:29:39 -0400

    features list colors

--- scripts/snippets/features-list.js
+++ scripts/snippets/features-list.js
@@ -1,22 +1,29 @@
 import Flickity from 'flickity';
 
-document.addEventListener('DOMContentLoaded', () => {
-  if (document.querySelector('#featuresListSlider')) {
-    let flkty = new Flickity('#featuresListSlider', {
-      pageDots: false,
-      prevNextButtons: true,
-      fade: true,
-      autoPlay: 5000,
-      initialIndex: 0,
-      pauseAutoPlayOnHover: true,
-      watchCSS: true,
-      on: {
-        ready: () => {
 
-        },
-        change: () => {
-        },
-      }
-    });
+const options = {
+  pageDots: false,
+  prevNextButtons: true,
+  fade: true,
+  autoPlay: 5000,
+  initialIndex: 0,
+  pauseAutoPlayOnHover: true,
+  watchCSS: true,
+  on: {
+    ready: () => {
+
+    },
+    change: () => {
+    },
   }
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  document.querySelectorAll('.features-list').forEach(el => {
+
+
+    let flkty = new Flickity(el.querySelector('.features-list__items'), options);
+
+  });
 });
