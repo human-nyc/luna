46b333514fe61603e5dd52edea450ead4d08f65d
michaelburtonray <michael@human-nyc.com>
2020-09-17 13:18:22 -0400

    products slider

--- scripts/snippets/product-slider.js
+++ scripts/snippets/product-slider.js
@@ -6,11 +6,10 @@ document.querySelectorAll('.products-slider__main').forEach(el => {
   const flkty = new Flickity(el, {
     pageDots: false,
     prevNextButtons: true,
-    fade: true,
     autoPlay: 5000,
     initialIndex: 1,
     pauseAutoPlayOnHover: true,
-    wrapAround: false,
+    wrapAround: true,
     on: {
       ready: () => {
 
