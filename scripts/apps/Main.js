c17d57a6a6479a4cfac1ffce8c2f4d96a4be4a14
sashakluchnik <me@sashakluchnik.com>
2020-09-23 15:14:21 -0400

    closes#209

--- scripts/apps/Main.js
+++ scripts/apps/Main.js
@@ -2,15 +2,26 @@ import Vue from 'vue';
 import store from '../store';
 
 document.addEventListener('DOMContentLoaded', () => {
+
   // Collection Page
   if (document.querySelector('#collection-page')) {
     const { id } = document.querySelector('#collection-page');
-
     new Vue({
       store,
       el: `#${id}`,
       delimiters: ['${', '}'],
       name: 'Collection page',
+    });
+  }
+  // Product Suggestion Section
+  if (document.querySelector('.product-item')) {
+    document.querySelectorAll('.product-item').forEach((el) => {
+      new Vue({
+        el,
+        delimiters: ['${', '}'],
+        store,
+        name: 'Product item',
+      })
     });
   }
 });
