80636eb62b45a1d32a168e7c998e621a4b6e3e3a
michaelburtonray <michael@human-nyc.com>
2020-09-28 12:55:56 -0400

    saves dark mode as a session variable

--- scripts/sections/footer.js
+++ scripts/sections/footer.js
@@ -1,20 +1,35 @@
+var colorSchemeIsDark;
+
 document.addEventListener('DOMContentLoaded', () => {
-  const mediaQueryList = matchMedia('(prefers-color-scheme: dark)');
-  darkModeTest(mediaQueryList);
-  mediaQueryList.addListener(darkModeTest);
+  if (sessionStorage.getItem("colorSchemeIsDark")) {
+    colorSchemeIsDark = (sessionStorage.getItem("colorSchemeIsDark") === 'true');
+
+    document.documentElement.classList.toggle('color-scheme--dark', colorSchemeIsDark);
+  } else {
+    const mediaQueryList = matchMedia('(prefers-color-scheme: dark)');
+
+    darkModeTest(mediaQueryList);
+  }
 
   const lightSwitchButtonEl = document.querySelector('#shopify-section-footer #light-switch-toggle');
 
   lightSwitchButtonEl.addEventListener('click', () => {
-    document.documentElement.classList.toggle('color-scheme--dark');
+    colorSchemeIsDark = !colorSchemeIsDark;
+
+    document.documentElement.classList.toggle('color-scheme--dark', colorSchemeIsDark);
+
+    sessionStorage.colorSchemeIsDark = colorSchemeIsDark;
   });
 });
 
-
 function darkModeTest({matches}) {
   if(matches) {
+    colorSchemeIsDark = true;
+
     document.documentElement.classList.add('color-scheme--dark');
   } else {
+    colorSchemeIsDark = false;
+
     document.documentElement.classList.remove('color-scheme--dark');
   }
 }
