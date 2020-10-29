fc8881159e8619b0adc8ba5e744fd71d8462a820
michaelburtonray <michael@human-nyc.com>
2020-10-29 09:07:34 -0400

    home page

--- scripts/templates/index.js
+++ scripts/templates/index.js
@@ -1,8 +1,5 @@
-// import '../apps/MiniProduct';
-
 import '../sections/comparison-chart';
 import '../sections/press';
 import '../sections/testimonials';
 
-import '../snippets/product-slider';
 import '../snippets/features-list';
