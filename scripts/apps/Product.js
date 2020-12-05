f4a9085aca4bb6c1e5c932cb8497ccc68007f5df
sashakluchnik <me@sashakluchnik.com>
2020-12-04 19:04:30 -0500

    rsticky add to cart reworked

--- scripts/apps/Product.js
+++ scripts/apps/Product.js
@@ -30,6 +30,7 @@ document.addEventListener('DOMContentLoaded', () => {
         selectedWeight: '',
         selecting: 'color',
         sizes: [],
+        stickyAddToCartIsActive: false,
         tab: 'benefits',
         weights: [],
       },
@@ -51,6 +52,30 @@ document.addEventListener('DOMContentLoaded', () => {
             this.toggleActiveSizePopup();
           });
         }
+        
+        const stickyAddToCart = document.querySelector('#stickyAddToCart');
+        const stickyAddToCartOffsetTop = stickyAddToCart.offsetTop;
+
+        const checkScrollPosition = () => {
+          
+          if ( !this.stickyAddToCartIsActive && scrollY >= stickyAddToCartOffsetTop ) {
+            requestAnimationFrame(() => {
+              this.stickyAddToCartIsActive = true
+              stickyAddToCart.classList.add('active');
+            });
+          }
+          if ( this.stickyAddToCartIsActive && scrollY < stickyAddToCartOffsetTop ) {
+            requestAnimationFrame(() => {
+              this.stickyAddToCartIsActive = false
+              stickyAddToCart.classList.remove('active');
+            });
+          }
+        } 
+
+        window.addEventListener('scroll', () => {
+          checkScrollPosition();
+        });
+
       },
       mixins: [productOptions],
       methods: {
@@ -60,6 +85,7 @@ document.addEventListener('DOMContentLoaded', () => {
           'setHasUpsell',
           'setUpsell',
           'toggleMiniCart',
+          'toggleStickyAddToCart',
         ]),
         ...mapActions('popups', ['openSizePopup', 'openWeightPopup']),
 
@@ -133,132 +159,132 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
-  if (document.querySelector('#stickyAddToCart')) {
-    new Vue({
-      name: "stickyAddToCart",
-      store,
-      delimiters: ['${', '}'],
-      el: '#stickyAddToCart',
-      data: {
-        activeOptionIdx: null,
-        colors: [],
-        currentVariant: null,
-        options: [],
-        optionsWithValues: [],
-        product: {
-          featured_image: "",
-          variants: [],
-        },
-        quantity: 1,
-        selectedColor: '',
-        selectedSize: '',
-        selectedWeight: '',
-        selecting: 'color',
-        sizes: [],
-        tab: 'benefits',
-        weights: [],
-      },
-      mounted() {
+  // if (document.querySelector('#stickyAddToCart')) {
+  //   new Vue({
+  //     name: "stickyAddToCart",
+  //     store,
+  //     delimiters: ['${', '}'],
+  //     el: '#stickyAddToCart',
+  //     data: {
+  //       activeOptionIdx: null,
+  //       colors: [],
+  //       currentVariant: null,
+  //       options: [],
+  //       optionsWithValues: [],
+  //       product: {
+  //         featured_image: "",
+  //         variants: [],
+  //       },
+  //       quantity: 1,
+  //       selectedColor: '',
+  //       selectedSize: '',
+  //       selectedWeight: '',
+  //       selecting: 'color',
+  //       sizes: [],
+  //       tab: 'benefits',
+  //       weights: [],
+  //     },
+  //     mounted() {
 
 
-        this.product = JSON.parse(document.querySelector('#ProductHero').dataset.product);
+  //       this.product = JSON.parse(document.querySelector('#ProductHero').dataset.product);
 
 
-        this.currentVariant = JSON.parse(document.querySelector('#ProductHero').dataset.currentVariant);
-        this.optionsWithValues = JSON.parse(document.querySelector('#ProductHero').dataset.optionsWithValues);
+  //       this.currentVariant = JSON.parse(document.querySelector('#ProductHero').dataset.currentVariant);
+  //       this.optionsWithValues = JSON.parse(document.querySelector('#ProductHero').dataset.optionsWithValues);
 
-        if (this.currentVariant && this.currentVariant.options) {
-          this.options = this.currentVariant.options;
-        }
+  //       if (this.currentVariant && this.currentVariant.options) {
+  //         this.options = this.currentVariant.options;
+  //       }
 
-        if (document.querySelector('#shopify-section-size-popup')) {
-          document.querySelector('#shopify-section-size-popup .size-popup__background').addEventListener('click', event => {
-            this.toggleActiveSizePopup();
-          });
+  //       if (document.querySelector('#shopify-section-size-popup')) {
+  //         document.querySelector('#shopify-section-size-popup .size-popup__background').addEventListener('click', event => {
+  //           this.toggleActiveSizePopup();
+  //         });
 
-          document.querySelector('#shopify-section-size-popup #size-popup__close-button').addEventListener('click', event => {
-            this.toggleActiveSizePopup();
-          });
-        }
-      },
-      mixins: [productOptions],
-      methods: {
-        ...mapActions('cart', [
-          'addToCart',
-          'hydrateCartItems',
-          'setHasUpsell',
-          'setUpsell',
-          'toggleMiniCart',
-        ]),
-        ...mapActions('popups', ['openSizePopup', 'openWeightPopup']),
+  //         document.querySelector('#shopify-section-size-popup #size-popup__close-button').addEventListener('click', event => {
+  //           this.toggleActiveSizePopup();
+  //         });
+  //       }
+  //     },
+  //     mixins: [productOptions],
+  //     methods: {
+  //       ...mapActions('cart', [
+  //         'addToCart',
+  //         'hydrateCartItems',
+  //         'setHasUpsell',
+  //         'setUpsell',
+  //         'toggleMiniCart',
+  //       ]),
+  //       ...mapActions('popups', ['openSizePopup', 'openWeightPopup']),
 
-        async submit(e) {
-          e.preventDefault();
-          const { id } = this.selectedVariant;
-          const properties = {};
-          const quantity = this.quantity;
-          const cartData = { id, properties, quantity };
+  //       async submit(e) {
+  //         e.preventDefault();
+  //         const { id } = this.selectedVariant;
+  //         const properties = {};
+  //         const quantity = this.quantity;
+  //         const cartData = { id, properties, quantity };
 
-          await this.addToCart(cartData);
-          await this.hydrateCartItems();
-          this.setUpsellBlock();
-          this.toggleMiniCart();
-        },
+  //         await this.addToCart(cartData);
+  //         await this.hydrateCartItems();
+  //         this.setUpsellBlock();
+  //         this.toggleMiniCart();
+  //       },
 
-        selectTab: function (tab) {
-          this.tab = tab;
-        },
+  //       selectTab: function (tab) {
+  //         this.tab = tab;
+  //       },
 
-        adjustQuantity: function (increment) {
-          this.quantity += increment;
-          if (this.quantity < 1) {
-            this.quantity = 1;
-          }
-        },
+  //       adjustQuantity: function (increment) {
+  //         this.quantity += increment;
+  //         if (this.quantity < 1) {
+  //           this.quantity = 1;
+  //         }
+  //       },
 
-        focusOption: function (option) {
-          this.selecting = option;
-        },
+  //       focusOption: function (option) {
+  //         this.selecting = option;
+  //       },
 
-        sizeName: function (size) {
-          return size.split(' ')[0].toLowerCase();
-        },
+  //       sizeName: function (size) {
+  //         return size.split(' ')[0].toLowerCase();
+  //       },
 
-        sizeDimensions: function (size) {
-          return size.match(/\((.+?)\)/)[1];
-        },
+  //       sizeDimensions: function (size) {
+  //         return size.match(/\((.+?)\)/)[1];
+  //       },
 
-        replaceHistoryState() {
-          if (!this.currentVariant) {
-            return;
-          }
-          let getVars = querystring.parse(location.search.substring(1));
-          getVars.variant = this.currentVariant.id;
-          const url = `${window.location.pathname}?${querystring.stringify(getVars)}`;
-          history.replaceState({}, '', url);
-        },
-      },
-      computed: {
-        ...mapGetters('cart', ['cartItems', 'hasUpsell', 'itemsWithUpsell', 'upsell']),
-        crop() {
-          if (window.matchMedia('(max-width: 767px)').matches) {
-            return '420x252';
-          } else {
-            return '840x840'
-          }
-        },
-      },
-      filters: {
-        formatMoney(price, format) {
-          return formatMoney(price, '${{amount}}');
-        },
+  //       replaceHistoryState() {
+  //         if (!this.currentVariant) {
+  //           return;
+  //         }
+  //         let getVars = querystring.parse(location.search.substring(1));
+  //         getVars.variant = this.currentVariant.id;
+  //         const url = `${window.location.pathname}?${querystring.stringify(getVars)}`;
+  //         history.replaceState({}, '', url);
+  //       },
+  //     },
+  //     computed: {
+  //       ...mapGetters('cart', ['cartItems', 'hasUpsell', 'itemsWithUpsell', 'upsell']),
+  //       crop() {
+  //         if (window.matchMedia('(max-width: 767px)').matches) {
+  //           return '420x252';
+  //         } else {
+  //           return '840x840'
+  //         }
+  //       },
+  //     },
+  //     filters: {
+  //       formatMoney(price, format) {
+  //         return formatMoney(price, '${{amount}}');
+  //       },
 
-        getSizedImageUrl(url, size) {
-          return getSizedImageUrl(url, size);
-        },
-      },
-    });
-  }
+  //       getSizedImageUrl(url, size) {
+  //         return getSizedImageUrl(url, size);
+  //       },
+  //     },
+  //   });
+  // }
 
 
 
--- scripts/templates/product.js
+++ scripts/templates/product.js
@@ -1,7 +1,7 @@
 import '../snippets/features-list';
 import '../snippets/product-steps';
-import ProductStickyAddToCart from '../snippets/product-sticky-add-to-cart';
+// import ProductStickyAddToCart from '../snippets/product-sticky-add-to-cart';
 
-document.addEventListener('DOMContentLoaded', () => {
-  new ProductStickyAddToCart();
-});
+// document.addEventListener('DOMContentLoaded', () => {
+//   new ProductStickyAddToCart();
+// });
